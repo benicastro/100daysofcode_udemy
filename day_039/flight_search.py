45f9fe17b3a4dded9c85de2ce5f43b2026b4e42d
Benedict Castro <109434459+benicastro@users.noreply.github.com>
2023-08-29 22:52:51 +0800

    Update flight_search.py by adding the check_flights function

--- day_039/flight_search.py
+++ day_039/flight_search.py
@@ -1,5 +1,6 @@
 # Import needed modules/libraries #######
 import requests
+from flight_data import FlightData
 
 # Create constant/global variables #######
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
@@ -19,3 +20,46 @@ class FlightSearch:
         code = results[0]["code"]
 
         return code
+
+    def check_flights(
+        self, origin_city_code, destination_city_code, from_time, to_time
+    ):
+        """Returns the flight data in dictionary format {flight_data.destination_city}: £{flight_data.price}"""
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP",
+        }
+
+        response = requests.get(
+            url=f"{TEQUILA_ENDPOINT}/v2/search",
+            headers=headers,
+            params=query,
+        )
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0],
+        )
+
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
