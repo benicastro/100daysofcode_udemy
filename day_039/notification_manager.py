6ddca1cf925acf9e16600098dc24f2de1bdc47d2
Benedict Castro <109434459+benicastro@users.noreply.github.com>
2023-08-29 23:14:35 +0800

    Updated notification_manager.py to incorporate twilio in the sms notification system

--- day_039/notification_manager.py
+++ day_039/notification_manager.py
@@ -1,3 +1,25 @@
+# Import needed modules/libraries #######
+from twilio.rest import Client
+
+# Create constant/global variables #######
+TWILIO_SID = "YOUR TWILIO SID"
+TWILIO_AUTH_TOKEN = "YOUR TWILIO AUTH TOKEN"
+TWILIO_VIRTUAL_NUMBER = "YOUR TWILIO VIRTUAL NUMBER"
+TWILIO_VERIFIED_NUMBER = "YOUR TWILIO VERIFIED NUMBER"
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    # This class is responsible for sending notifications with the deal flight details.
+
+    def __init__(self):
+        self.client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, message):
+        """Sends an sms notification to a valid phone number"""
+        message = self.client.messages.create(
+            body=message,
+            from_=TWILIO_VIRTUAL_NUMBER,
+            to=TWILIO_VERIFIED_NUMBER,
+        )
+        # Prints if successfully sent.
+        print(message.sid)
